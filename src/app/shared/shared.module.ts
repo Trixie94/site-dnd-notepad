9848af8d0f51b6d2bfcabd8a659547c052159bd9
mateusz.gorczycki <mateusz.gorczycki@whiteaster.com>
2023-01-22 12:28:34 +0000

    Add new header

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+	imports: [CommonModule],
+	exports: [HeaderComponent],
+	declarations: [HeaderComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
